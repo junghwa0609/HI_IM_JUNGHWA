e0cf581ff6a00cc17daaf12ff93fefb4696e07d8
junghwa0609 <hayan094666@gmail.com>
2024-07-05 14:17:26 +0900

    업데이트
    이름, 파일이름, 상반기 하반기, 2023까지만 크롤되는거 (while 종료 오류) 해결

--- 나라장터 크롤링/크롤링.py
+++ 나라장터 크롤링/크롤링.py
@@ -12,7 +12,7 @@ period = {
     '하반기끝': '/12/31'
 }
 
-def page_scrapping(page_number, year, start, end):
+def page_scrapping(page_number, company, year, start, end):
     url = "https://www.g2b.go.kr:8101/ep/tbid/tbidList.do"
     payload = {
         "area": "",
@@ -20,7 +20,7 @@ def page_scrapping(page_number, year, start, end):
         "bidSearchType": "1",
         "fromBidDt": f"{year}{start}",
         "fromOpenBidDt": "",
-        "instNm": "한국은행",
+        "instNm": f"{company}",
         "maxPageViewNoByWshan": "6",
         "radOrgan": "1",
         "regYn": "Y",
@@ -35,39 +35,44 @@ def page_scrapping(page_number, year, start, end):
     query_string = urlencode(payload, encoding='euc-kr')
     full_url = f"{url}?{query_string}"
     
-    response = requests.get(full_url)
-    
-    if response.status_code == 200:
-        response.encoding = 'euc-kr'
-        html_content = response.text
-        html = BeautifulSoup(html_content, 'html.parser')
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
 
-        content_link = []
-
-        for i in html.select('td.tl > div > a'):
-            link = i['href']
-            content_link.append(link)
-        
-        html_string = StringIO(html_content)
-        
-        try:
-            df_list = pd.read_html(html_string)
-            df = df_list[0]
-        except ValueError as e:
-            print(f"테이블을 찾을 수 없습니다: {e}")
-            return None
-
-        if (df['업무'] == "검색된 데이터가 없습니다.").all():
-            return None
-        else:
-            df = df[df['업무'].notnull()].copy()
-            df.loc[:, "내용링크"] = content_link[:len(df)]
-            return df
-    else:
-        print(f"요청 실패, 상태 코드: {response.status_code}")
+    try:
+        response = requests.get(full_url, headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"요청 실패: {e}")
         return None
 
-def scrape_until_end(start_year, start_period):
+    response.encoding = 'euc-kr'
+    html_content = response.text
+    html = BeautifulSoup(html_content, 'html.parser')
+
+    content_link = []
+
+    for i in html.select('td.tl > div > a'):
+        link = i['href']
+        content_link.append(link)
+    
+    html_string = StringIO(html_content)
+    
+    try:
+        df_list = pd.read_html(html_string)
+        df = df_list[0]
+    except ValueError as e:
+        print(f"테이블을 찾을 수 없습니다: {e}")
+        return None
+
+    if (df['업무'] == "검색된 데이터가 없습니다.").all():
+        return None
+    else:
+        df = df[df['업무'].notnull()].copy()
+        df.loc[:, "내용링크"] = content_link[:len(df)]
+        return df
+
+def scrape_until_end(start_year, company, start_period):
     page_number = 1
     all_data = pd.DataFrame()
     current_date = datetime.now()
@@ -76,28 +81,36 @@ def scrape_until_end(start_year, start_period):
     
     start_idx = period_keys.index(start_period)
     
-    while True:
+    while year <= current_date.year:
         for i in range(start_idx, len(period_keys), 2):
             start_day = period[period_keys[i]]
             end_day = period[period_keys[i + 1]]
             
+            if year == current_date.year:
+                if i == 0 and current_date.strftime('%m%d') <= period['상반기끝'].replace('/',''): #i==0 상반기
+                    end_day = current_date.strftime('/%m/%d')
+                elif i == 2 and current_date.strftime('%m%d') > period['상반기끝'].replace('/',''): #i==2 하반기
+                    end_day = current_date.strftime('/%m/%d')
+                elif i == 2 and current_date.strftime('%m%d') <= period['상반기끝'].replace('/',''):
+                    break  # 현재 연도의 하반기가 아직 시작되지 않았으면 루프 종료
+            
             while True:
-                result = page_scrapping(page_number, year, start_day, end_day)
+                result = page_scrapping(page_number, company, year, start_day, end_day)
                 if result is None:
                     break
                 else:
                     all_data = pd.concat([all_data, result], ignore_index=True)
                 page_number += 1
             
-            page_number = 1  # Reset page number for the next period
+            page_number = 1  # 다음 6개월을 위해 페이지 넘버 리셋
+        
+        if year == current_date.year:
+            break  # 현재 연도의 데이터를 모두 수집했으면 루프 종료
         
         year += 1
-        start_idx = 0  # Reset to start from the first period of the next year
+        start_idx = 0  # 다음 년도를 위해 period 번호 리셋
         
-        if year > current_date.year or (year == current_date.year and period_keys[i + 1] > current_date.strftime('/%m/%d')):
-            break
+    all_data.to_csv(f'{company}.csv', index=False, encoding='utf-8-sig')
+    print(f"{start_year}/01/01 부터 {current_date.strftime('%Y/%m/%d')} 까지의 모든 데이터를 CSV 파일로 저장했습니다.")
 
-    all_data.to_csv('한국은행.csv', index=False, encoding='utf-8-sig')
-    print("모든 데이터를 CSV 파일로 저장했습니다.")
-
-scrape_until_end(2022, '상반기시작') #임의로 넣어본..
+scrape_until_end(2021, '전력거래소', '상반기시작')
